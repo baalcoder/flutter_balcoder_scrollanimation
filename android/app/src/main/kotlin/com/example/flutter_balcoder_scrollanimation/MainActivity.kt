fb9ee1be64fbe3b68c6ba3248dc1eb39af3345cc
Balcoder <balcode.software@gmail.com>
2022-06-30 18:08:19 -0500

    full logic

---
+++ android/app/src/main/kotlin/com/example/flutter_balcoder_scrollanimation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_balcoder_scrollanimation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/content_widget.dart
@@ -0,0 +1,71 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class ContentWidget extends StatefulWidget {
+  final String? src;
+
+  const ContentWidget({Key? key, this.src}) : super(key: key);
+
+  @override
+  _ContentWidgetState createState() => _ContentWidgetState();
+}
+
+class _ContentWidgetState extends State<ContentWidget> {
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController;
+  bool _liked = false;
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer();
+  }
+
+  Future initializePlayer() async {
+    _videoPlayerController = VideoPlayerController.network(widget.src!);
+    await Future.wait([_videoPlayerController.initialize()]);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: true,
+      showControls: false,
+      looping: true,
+    );
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        _chewieController != null &&
+                _chewieController!.videoPlayerController.value.isInitialized
+            ? GestureDetector(
+                onDoubleTap: () {
+                  setState(() {
+                    _liked = !_liked;
+                  });
+                },
+                child: Chewie(
+                  controller: _chewieController!,
+                ),
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 10),
+                  Text('Loading...')
+                ],
+              ),
+      ],
+    );
+  }
+}
